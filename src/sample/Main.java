27ad214e9ec73109f81bf9bb0b6501ba9f411231
Jose Andres <joseandres216@gmail.com>
2020-02-24 17:12:02 -0600

    Almost done the graphic interface

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent rootL = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent rootMS = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
         primaryStage.setTitle("Login");
-        Scene login = new Scene(root, 380, 200);
+        Scene login = new Scene(rootL);
         primaryStage.setScene(login);
+
+        primaryStage.setTitle("LocalChat by Jose Andres Rodriguez");
+        Scene mainscreen = new Scene(rootMS);
+        primaryStage.setScene(mainscreen);
         primaryStage.setResizable(false);
         primaryStage.show();
     }
