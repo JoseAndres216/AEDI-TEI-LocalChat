7574cb33cb98077156aeabd272903898a117dfaf
Jose Andres <joseandres216@gmail.com>
2020-02-25 17:40:18 -0600

    Done the Persona class (fix #7 and fix #8)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
@@ -25,7 +27,6 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Main.main");
         launch(args);
     }
 }
---
+++ src/sample/Persona.java
@@ -0,0 +1,45 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Persona{
+    private int puerto;
+
+    public Persona(int puerto){
+        this.puerto = puerto;
+    }
+
+    public void enviar_mensaje(String Mensaje, int puerto_meta){
+        try {
+            System.out.println("Entro");
+            Socket client = new Socket("127.0.0.1", puerto_meta);
+            OutputStreamWriter writer = new OutputStreamWriter(client.getOutputStream());
+            writer.write(Mensaje);
+            writer.flush();
+            client.close();}catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void escuchar(){
+        boolean active = true;
+        try{
+            ServerSocket server = new ServerSocket(this.puerto);
+            while(active){
+                System.out.println("Listen");
+                Socket entrante = server.accept();
+                BufferedReader lector = new BufferedReader(new InputStreamReader(entrante.getInputStream()));
+                String mensaje_recibido = lector.readLine();
+                System.out.println("Mensaje: " + mensaje_recibido);
+                entrante.close();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
